8a68752ec2799a4e25839aa0e9a046e4e79cbaa2
neel <neelbarvaliya04@gmail.com>
2024-03-03 16:26:15 +0530

    made few improvements in the html and added validations for the card name and card number

---
+++ src/main.js
@@ -0,0 +1,129 @@
+"use strict";
+
+// card--display
+const cardNumberEl = document.querySelector(".card--number");
+const cardNameEl = document.querySelector(".card--name");
+const expDateEl = document.querySelector(".exp--date");
+const cvcCodeEl = document.querySelector(".cvc--code");
+
+// input
+const inputNameEl = document.querySelector(".input--name");
+const inputNumberEl = document.querySelector(".input--number");
+const inputMonthEl = document.querySelector(".input--month");
+const inputYearEl = document.querySelector(".input--year");
+const inputCvcEl = document.querySelector(".input--cvc");
+const inputAllEl = document.querySelectorAll("input");
+
+// btn
+const btnEl = document.querySelector(".btn");
+
+// error
+const errorAllEl = document.querySelectorAll(".error");
+
+// regular expression
+const letterRegex = new RegExp(/[a-zA-Z ]/);
+const numRegex = new RegExp(/[0-9 ]/);
+
+// array for card holder name,number
+let keysPressed = [];
+let numsPressed = [];
+
+// error message
+const errorMessage = {
+  empty: "*Please enter this field",
+};
+
+// ********************************
+// conform Btn Clicked
+// ********************************
+btnEl.addEventListener("click", function (e) {
+  // checking for empty fields
+  if (!emptyInput(inputAllEl)) {
+    console.log("fuck");
+  }
+});
+
+// ********************************
+// ENTRING CARD NAME
+// ********************************
+inputNameEl.addEventListener("keydown", function (e) {
+  //   e.preventDefault();
+  const keyPressed = e.key;
+
+  //   when user deletes a letter
+  if (keyPressed === "Backspace") {
+    formatStr(keysPressed.pop(), keysPressed, cardNameEl);
+  }
+  if (correctKeyStr(keyPressed)) {
+    formatStr(keysPressed.push(keyPressed), keysPressed, cardNameEl);
+  } else {
+    console.log("fuck");
+  }
+});
+
+// ********************************
+// ENTRING CARD Number
+// ********************************
+inputNumberEl.addEventListener("keydown", function (e) {
+  const keyPressed = e.key;
+  if (keyPressed === "Backspace") {
+    formatStr(numsPressed.pop(), numsPressed, cardNumberEl);
+  }
+  if (correctKeyNum(keyPressed)) {
+    formatStr(numsPressed.push(keyPressed), numsPressed, cardNumberEl);
+  }
+});
+
+// ********************************
+// formatting the entered key
+// ********************************
+function formatStr(type, arr, el) {
+  type;
+  el.textContent = arr.join("");
+}
+
+// ********************************
+// Check if the pressed key is a letter
+// ********************************
+function correctKeyStr(keyPressed) {
+  let isLetter = true;
+  if (!letterRegex.test(keyPressed) || keyPressed.length > 1) {
+    isLetter = false;
+  }
+  return isLetter;
+}
+
+// ********************************
+// Check if the pressed key is a number
+// ********************************
+function correctKeyNum(keyPressed) {
+  let isNum = true;
+  if (!numRegex.test(keyPressed) || keyPressed.length > 1) {
+    isNum = false;
+  }
+  return isNum;
+}
+
+// ********************************
+// funnction if empty input
+// ********************************
+function emptyInput(inputs) {
+  let hasValue = true;
+  [...inputs].forEach(function (e) {
+    const value = e.value.trim();
+    if (value === "" || value === null || Number(value) === 0) {
+      hasValue = false;
+      setError(e, errorMessage.empty);
+    }
+  });
+  return hasValue;
+}
+
+// ********************************
+// setting error correctly
+// ********************************
+function setError(input, errorMsg) {
+  const parent = input.parentElement;
+  const errorEl = parent.querySelector(".error");
+  errorEl.textContent = errorMsg;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,15 @@ module.exports = {
       maxWidth: {
         card: "28rem",
       },
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        Light_grayish_violet: "hsl(270, 3%, 87%)",
+        Dark_grayish_violet: "hsl(279, 6%, 55%)",
+        Very_dark_violet: "hsl(278, 68%, 11%)",
+      },
     },
     fontFamily: {
       space: ["Space Grotesk", "sans-serif"],
-    },
-    colors: {
-      white: "hsl(0, 0%, 100%)",
-      Light_grayish_violet: "hsl(270, 3%, 87%)",
-      Dark_grayish_violet: "hsl(279, 6%, 55%)",
-      Very_dark_violet: "hsl(278, 68%, 11%)",
     },
   },
   plugins: [
@@ -22,6 +22,9 @@ module.exports = {
       addComponents({
         ".custom-label": {
           "@apply block mb-2 text-[0.95rem] tracking-[2.5px]": true,
+        },
+        ".error": {
+          "@apply invisible text-xs text-red-600 mt-[0.2rem]": true,
         },
       });
     },
