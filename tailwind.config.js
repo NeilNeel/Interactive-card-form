b925db32ee556636c051c08f723e778d747d270d
neel <neelbarvaliya04@gmail.com>
2024-03-03 14:24:06 +0530

    completed the design of the page with css

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
     fontFamily: {
       space: ["Space Grotesk", "sans-serif"],
     },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      Light_grayish_violet: "hsl(270, 3%, 87%)",
+      Dark_grayish_violet: "hsl(279, 6%, 55%)",
+      Very_dark_violet: "hsl(278, 68%, 11%)",
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".custom-label": {
+          "@apply block mb-2 text-[0.95rem] tracking-[2.5px]": true,
+        },
+      });
+    },
+  ],
 };
